b30af602607515a08b6da61e400700940493358b
artie-sh-weather <artem.shendrikov.contractor@weather.com>
2018-11-06 19:37:31 +0100

    graph done

--- apps/app.py
+++ apps/app.py
@@ -3,6 +3,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import dateparser
+import plotly.graph_objs as go
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -10,27 +12,41 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
-}
+    }
 
 df = pd.read_csv('./xrp.csv')
+
+# def avg(values):
+#     return sum(values)/len(values)
+
+def getDataRows(df):
+    coords = dict()
+    for row in df.iterrows():
+        r = list(row[1])
+        coords[dateparser.parse(r[0])] = r[2]
+    return coords
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-def generate_table(dataframe):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+def generate_graph(dataframe):
+    pass
 
-        # Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(len(dataframe))]
-    )
+coords = getDataRows(df)
 
 app.layout = html.Div(children=[
     html.H4(children='artie test table'),
-    generate_table(df)
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': sorted(coords.keys()), 'y': [coords[key] for key in sorted(coords.keys())], 'type': 'linear', 'name': 'XRP/USD'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
 if __name__ == '__main__':
--- apps/exps.py
+++ apps/exps.py
@@ -1,6 +1,20 @@
 import pandas as pd
+import dateparser
 
-df = pd.read_csv('./xrp.csv')
+df = pd.read_csv('./xrp1.csv')
 
-for row in df:
-    print row
+# def avg(values):
+#     return sum(values)/len(values)
+
+def getData(df):
+    coords = dict()
+    for row in df.iterrows():
+        r = list(row[1])
+
+        coords[dateparser.parse(r[0])] = r[2]
+
+    for item in sorted(coords.keys()):
+        print '%s - %s' % (item, coords[item])
+
+
+getData(df)
