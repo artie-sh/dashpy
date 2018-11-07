edcacd37952d9d283992fb805ea6cf4dd74d418d
artie-sh <artie.sh.87@gmail.com>
2018-11-07 02:02:51 +0100

    db added

--- apps/app.py
+++ apps/app.py
@@ -4,6 +4,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import dateparser
+from database_writer import CsvReader
+
 import plotly.graph_objs as go
 
 
@@ -14,16 +16,16 @@ colors = {
     'text': '#7FDBFF'
     }
 
-df = pd.read_csv('./xrp.csv')
+#df = pd.read_csv('./xrp.csv')
+reader = CsvReader()
+df = reader.read_db(reader.connect_db(reader.db_path))
 
-# def avg(values):
-#     return sum(values)/len(values)
+columns = {'date': 0, 'open': 1, 'high': 2, 'low': 3, 'close': 4}
 
-def getDataRows(df):
+def getDataRows(df, column_name):
     coords = dict()
-    for row in df.iterrows():
-        r = list(row[1])
-        coords[dateparser.parse(r[0])] = r[2]
+    for row in df:
+        coords[row[0]] = row[columns[column_name]]
     return coords
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -32,15 +34,21 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 def generate_graph(dataframe):
     pass
 
-coords = getDataRows(df)
+coords_open = getDataRows(df, 'open')
+coords_high = getDataRows(df, 'high')
+coords_low = getDataRows(df, 'low')
+coords_close = getDataRows(df, 'close')
 
 app.layout = html.Div(children=[
-    html.H4(children='artie test table'),
+    html.H4(children='XRP/USD'),
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': sorted(coords.keys()), 'y': [coords[key] for key in sorted(coords.keys())], 'type': 'linear', 'name': 'XRP/USD'},
+                {'x': sorted(coords_open.keys()), 'y': [coords_open[key] for key in sorted(coords_open.keys())], 'type': 'linear', 'name': 'Open'},
+                {'x': sorted(coords_high.keys()), 'y': [coords_high[key] for key in sorted(coords_high.keys())], 'type': 'linear', 'name': 'High'},
+                {'x': sorted(coords_low.keys()), 'y': [coords_low[key] for key in sorted(coords_low.keys())], 'type': 'linear', 'name': 'Low'},
+                {'x': sorted(coords_close.keys()), 'y': [coords_close[key] for key in sorted(coords_close.keys())], 'type': 'linear', 'name': 'Close'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
@@ -48,6 +56,7 @@ app.layout = html.Div(children=[
         }
     )
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ apps/database_writer.py
@@ -0,0 +1,42 @@
+import sqlite3
+import dateparser
+
+
+class CsvReader:
+
+    db_path = '../prices.sqlite'
+    csv_path = 'xrp.csv'
+
+    def read_file(self, file_name):
+        formatted = list()
+        csv = open(file_name).read().splitlines()
+        for line in csv:
+            fline = dict()
+            line = line.split(',')
+            fline['date'] = dateparser.parse(line[0])
+            fline['open'] = line[2]
+            fline['high'] = line[3]
+            fline['low'] = line[4]
+            fline['close'] = line[5]
+            formatted.append(fline)
+        return formatted
+
+    def connect_db(self, db_path):
+        conn = sqlite3.connect(db_path)
+        return conn
+
+    def read_db(self, cur):
+        return list(cur.execute('SELECT * FROM xrp'))
+
+    def write_line(self, cur, line):
+        cur.execute('INSERT INTO xrp (date, open, high, low, close) VALUES (?, ?, ?, ?, ?)',
+                    (line['date'], line['open'], line['high'], line['low'], line['close']))
+
+    def write_csv_to_db(self, csv, db):
+        conn = self.connect_db(db)
+        cur = conn.cursor()
+        data = self.read_file(csv)
+        for line in data:
+            self.write_line(cur, line)
+        conn.commit()
+        conn.close()
